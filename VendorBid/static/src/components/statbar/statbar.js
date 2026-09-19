6e0a390d4cfaef66cd88907492785a8d6637ca72
Ashaful A. <95497643+ashaful@users.noreply.github.com>
2026-09-19 12:13:58 +0600

    Delete VendorBid directory

--- VendorBid/static/src/components/statbar/statbar.js
+++
@@ -1,7 +0,0 @@
-/** @odoo-module */
-
-const { Component } = owl;
-
-export class StatBar extends Component {
-    static template = 'supplies.statbar';
-}
--- VendorBid/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import supplies_blacklist_wizard
-from . import supplies_reject_application_wizard
-from . import supplies_rfp_report_wizard
-from . import partner_edit_reject_wizard
-from . import supplies_bid_decision_wizard
